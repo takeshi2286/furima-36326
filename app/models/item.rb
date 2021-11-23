563ed79bec9e7da92b77c718b03614a75308e335
takeshi2286 <takeshi.m17@gmail.com>
2021-11-23 21:27:34 +0900

    出品機能修正２

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,13 +3,13 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
-    validates :name, presence: true, length: { maximum: 40 }
-    validates :content, presence: true, length: { maximum: 1000 }
-    validates :category_id, presence: true, numericality: { other_than: 1 ,message: "can't be blank"}
-    validates :status_id, presence: true, numericality: { other_than: 1 ,message: "can't be blank"}
-    validates :shipping_fee_id, presence: true, numericality: { other_than: 1 ,message: "can't be blank"}
-    validates :prefecture_id, presence: true, numericality: { other_than: 1 ,message: "can't be blank"}
-    validates :derivery_id, presence: true, numericality: { other_than: 1 ,message: "can't be blank"}
-    validates :price , presence: true, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999} , format: { with: /\A[0-9]+\z/ }
+    validates :name, length: { maximum: 40 }
+    validates :content, length: { maximum: 1000 }
+    validates :category_id, numericality: { other_than: 1 ,message: "can't be blank"}
+    validates :status_id, numericality: { other_than: 1 ,message: "can't be blank"}
+    validates :shipping_fee_id, numericality: { other_than: 1 ,message: "can't be blank"}
+    validates :prefecture_id, numericality: { other_than: 1 ,message: "can't be blank"}
+    validates :derivery_id, numericality: { other_than: 1 ,message: "can't be blank"}
+    validates :price , numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999} , format: { with: /\A[0-9]+\z/ }
   end
 end
