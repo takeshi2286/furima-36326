d90abe5b3301606f0d489f8d5bbfcdc2c12ae82f
takeshi2286 <takeshi.m17@gmail.com>
2021-12-15 20:34:54 +0900

    comit

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe Order, type: :model do
       user = FactoryBot.create(:user)
       item = FactoryBot.create(:item)
       @order = FactoryBot.build(:order, user_id: user.id, item_id: item.id)
-      sleep(0.1)
     end
 
     context '購入がうまくいくとき' do
